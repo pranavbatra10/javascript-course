99c3041c75b26471b1782d9371eec888174364fb
pranavbatra10 <pranavbatra10@gmail.com>
2025-07-05 14:45:32 +0530

    Update and theory cart-oop.js

--- 2-copy-of-code/lesson-17/data/cart-oop.js
+++ 2-copy-of-code/lesson-17/data/cart-oop.js
@@ -1,10 +1,17 @@
+// oops- object oriented programming system, isme sara code objects mai daal dete h
+// 'let cart;'   this is a short cut for 'let cart = undefined;'
+// so, main points of converting a normal code to oops code, we just have to make objects and after every object, we have to add comma(,) and then add another object 
+// and  we have to use (this) for reference to the parent object
+// and we have to use object syntax like (property: value)
+
+// in oops we use pascal case - every word starts with a capital letter inc the first word
 function Cart(localStorageKey) {
   const cart = {
-    cartItems: undefined,
+    cartItems: undefined,          // from (export let cart = undefined;) to cartItems: undefined, // here we dont use export becayuse in object there is syntax as (property: value) 
 
-    loadFromStorage() {
-      this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));
-
+    loadFromStorage() {    // idhar bhi we removed 'export function loadFromStorage() {}' and convert it to syntax (property: value) so we converted it to `loadFromStorage: function() {}` then shortcut of this syntax is shorthand property so we dont have to specify function here and our code on left is shortcut     
+      this.cartItems = JSON.parse(localStorage.getItem(localStorageKey));    // 'this' refers to the outer object
+                                                                              // so other than cart.cartItems we refer this.cartItems so if the name cart changes, the code still works
       if (!this.cartItems) {
         this.cartItems = [{
           productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
